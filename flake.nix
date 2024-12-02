0fd4b825e4b02a48a0f69e2c29a1ac3fce13cfef
Hiten Tandon <tandonhiten@gamil.com>
2024-12-03 02:23:10 +0530

    Feat: Created a gleam project

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs-channels/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
-        devShells.default = mkShell { nativeBuildInputs = [ nushell ]; };
-        formatter = nixfmt-classic;
-      });
+  outputs = {nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system: 
+    with import nixpkgs {inherit system;}; {
+      devShells.default = mkShell {
+        nativeBuildInputs = [gleam erlang_27];
+      };
+      formatter = nixfmt-classic;
+    }
+  );
 }
